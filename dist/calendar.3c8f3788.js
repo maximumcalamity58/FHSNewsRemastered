7761c0132881fd446db3df9e6feb1e9d5308558c
MaximumCalamity <110183656+MaximumCalamity@users.noreply.github.com>
2023-08-29 10:24:05 -0400

    i have no idea what im doing
    nothing has changed

--- dist/calendar.3c8f3788.js
+++ dist/calendar.3c8f3788.js
@@ -292,7 +292,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55078" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57093" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
