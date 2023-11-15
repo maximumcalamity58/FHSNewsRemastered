2da82a0c36869f2ba79064013bb2a98d903e43b0
MaximumCalamity <110183656+MaximumCalamity@users.noreply.github.com>
2023-11-15 10:09:11 -0500

    i dont even know

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3003;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
