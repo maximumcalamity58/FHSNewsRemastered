b143ad0fdb9b05c0e518b883fc86f58af1b93325
MaximumCalamity <110183656+MaximumCalamity@users.noreply.github.com>
2023-12-07 08:19:46 -0500

    base.html is now the template and the app works again

--- server/settings.py
+++ server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = get_secret_key(KEY_FILE)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
