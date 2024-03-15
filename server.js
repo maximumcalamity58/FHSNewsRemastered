0d771156e45780ef0aeaad074c7ed443a3a562aa
maximumcalamity58 <maximehp@icloud.com>
2024-03-15 09:22:44 -0400

    node

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3005;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Specific route for /calendar
+app.get('/calendar', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/calendar.html'));
+});
+
+// Specific route for /calendar
+app.get('/clubs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/clubs.html'));
+});
+
+// Specific route for /calendar
+app.get('/info', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/info.html'));
+});
+
 
 // Catch-all route to handle all other requests and return the index.html file
 app.get('*', (req, res) => {
